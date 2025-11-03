1f96568a817f0cea5e70914c3d1f0f774232f074
mmenanno <1358708+mmenanno@users.noreply.github.com>
2025-11-03 08:26:38 -0500

    fix: add toInt64 template function for average file size display

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -2567,5 +2567,19 @@ func (s *Server) createTemplateFuncs() template.FuncMap {
 				return 0
 			}
 		},
+		"toInt64": func(v interface{}) int64 {
+			switch val := v.(type) {
+			case float64:
+				return int64(val)
+			case float32:
+				return int64(val)
+			case int64:
+				return val
+			case int:
+				return int64(val)
+			default:
+				return 0
+			}
+		},
 	}
 }
